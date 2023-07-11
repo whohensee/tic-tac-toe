61e0e62ae1ed5ad4caf74ef739c904417f089460
Extav <106775295+whohensee@users.noreply.github.com>
2023-07-11 16:05:36 -0700

    Add structure pseudocode

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tic-tac-toe
+
+  A functional game of Tic Tac Toe, playable in the browser.
+
--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+
+/* 
+----- Pseudocode -----
+
+- Gameboard Object
+  - The purpose of this object is to store all information related to the state
+    of the game board.
+
+  - array gameboardArray: stores the 9 spaces representing a 3x3 board
+  - placeMarker: places a given player's marker in an unoccupied board spot
+  - evaluateBoard: checks for a win or draw
+
+- Player Object
+  - stores information regarding the player
+
+  - string marker: X or O, the marker represented by the player
+
+- Game Object
+  - runs a game, including adding the board via javascript (might end up in
+    Gameboard object)
+
+
+*/
